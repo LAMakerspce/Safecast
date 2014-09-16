3248eb3dadc2ca75ca2cf87e9415fd115d6618cd
Ariel Levi Simons <levisimons@gmail.com>
2014-09-16 13:34:54 -0700

    Update rad.py

--- rad.py
+++ rad.py
@@ -10,8 +10,61 @@ import math
 import time
 import datetime
 import numpy
+import webbrowser
+import os, sys
+import operator
 
-input_file = csv.DictReader(open("measurements.csv"))
+i=0
+print "Make sure your browser downloads files to the directory this script is working in."
+print "You will also need the file with the Fukushima sites."
+print "Please make sure that FukushimaCensus.csv lives in the same directory as the script."
+id=int(raw_input('Enter site ID'))
+census_file = csv.DictReader(open("FukushimaCensus.csv"))
+for row in census_file:
+	site=int(row["ID"])
+	if site==id:
+		i=i+1
+		if i==1:
+			lat=float(row["Latitude"])
+			lon=float(row["Longitude"])
+			print site,lat,lon
+#This loop takes user input on a particular site by ID, then grabs the location information from the FukushimaCensus.csv file.
+
+url='https://api.safecast.org/en-US/measurements?utf8=%E2%9C%93&latitude={lat}&longitude={lon}&distance=100&captured_after=&captured_before=&since=&until=&commit=Filter&format=csv&options=with-headings'.format(lat=lat, lon=lon)
+print "Downloading all Safecast data within 100m of {lat}, {lon}".format(lat=lat, lon=lon)
+webbrowser.open(url)
+#Use the latitude and longitude values to download all of the Safecast data within 100m of the site latitude and longitude.
+
+path = os.getcwd()
+#Get current path
+#Make sure your browser downloads files to the directory this script is working in.
+
+downloadFile='{path}/measurements.csv'.format(path=path)
+while os.path.isfile(downloadFile)==False:
+	time.sleep(5)
+	print 'Downloading'
+else:
+	print 'Safecast data downloaded'
+	downloadFile='measurementsSite{id}.csv'.format(id=id)
+	os.rename('measurements.csv',downloadFile)
+#Wait while the data is being downloaded from Safecast.
+#When the file is finished downloading make sure to rename it to the site ID for future reference.
+
+dataSorter = csv.reader(open(downloadFile),delimiter=',')
+sortedlist = sorted(dataSorter, key=operator.itemgetter(0))
+header=["Captured Time", "Latitude", "Longitude", "Value", "Unit", "Location Name", "Device ID", "MD5Sum", "Height", "Surface", "Radiation", "Uploaded Time", "Loader ID"]
+with open("measurements.csv", "wb") as f:
+	fileWriter = csv.writer(f, delimiter=',')
+	fileWriter.writerow(header)
+	for row in sortedlist:
+		if row!=header: fileWriter.writerow(row)
+os.rename('measurements.csv',downloadFile)
+#This loop sorts the data downloaded from Safecast by collection time before analysis work is done.
+
+input_file = csv.DictReader(open(downloadFile))
+#Designate the input file and run analysis to determine the average and standard deviation on the radioactivity
+#levels for a given selection area, in this case a circle of 100m radius around the site, and day in duration.
+#The average radioactivity per site per day will be used to fit a contamination decay curve using an exponential decay model.
 
 i=0
 j=0
@@ -46,7 +99,8 @@ for row in input_file:
 	radList.append(rad)
 #This loop readings in all of the dates and radiation readings into two lists.
 
-fileWriter = open('radAnalysis.csv','wb')
+averageData='radAveragesSite{id}.csv'.format(id=id)
+fileWriter = open(averageData,'wb')
 wr = csv.writer(fileWriter)
 label = ["date","Unix clock(s)","Average radiation (cpm)","STDEV on radiation (cpm)"]
 wr.writerow(label)
@@ -81,18 +135,25 @@ for i in range(0, len(dateList)):
 x=numpy.array(unixClock)
 y=numpy.array(radReadings)
 coefficients=numpy.polyfit(x,y,1)
-print coefficients[0],coefficients[1]
+print "The contamination decay curve is R(t)=exp({a}*t + {b})".format(a=coefficients[0],b=coefficients[1])
+print "R(t) is in counts per minute and t is in Unix clock seconds"
 #Fit a linear function to the natural log of radioactivity versus time.
 #This can then be used to interpolate average radioactivity for the selection area for a given date.
 
 census_file = csv.DictReader(open("FukushimaCensus.csv"))
-id=int(raw_input('Enter site ID'))
+interpolated_file=csv.writer(open("radInterpolatedSite{id}.csv".format(id=id),"wb"))
+interpolated_file.writerow(["Site ID","Latitude","Longitude","Date","Unix clock (s)","Interpolated radioactivity (cpm)"])
+print "Site ID, Latitude, Longitude, Date, Unix clock (s), Interpolated radioactivity (cpm)"
+
 for row in census_file:
-	site=int(row["ID"])
-	
+	site=int(row["ID"])	
 	if site==id:
 		t=float(row["Unix Clock (s)"])
 		radInterpolated=math.exp(coefficients[0]*t+coefficients[1])
-		print site,row["Day"],row["Year"][:3],"20%s"%row["Year"][4:],t,radInterpolated
+		formattedDate = "/".join((dateList[i][5:7],dateList[i][8:10],dateList[i][:4]))
+		data=site,row["Latitude"],row["Longitude"],row["Date"],t,radInterpolated
+		interpolated_file.writerow(data)
+		print data
 #This loop opens up a list of sites around which selection area are taken.
 #Interpolated values are calculated from the different times at which a site has data.
+#The interpolation model uses an exponential best-fit based on the averaged radiation level for a selection area on a day-by-day basis.
